8e45f834bc8ff2fface92ddb232d827795c7fe8a
HAJ02 <Haj020206@gmail.com>
2025-11-20 13:33:13 -0500

    DotHandler formatted

---
+++ DotHandler.java
@@ -0,0 +1,30 @@
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+
+public class DotHandler {
+    public void writeDot(Graph e) {
+        //    try(BufferedWriter out= new BufferedWriter(new OutputStreamWriter(new FileOutputStream("Graph.dot")))){
+        //out.write("Directed Graph");
+        //  out.newLine();
+        //     for (int i = 0; i < maxVertices; i++) {
+        //         if (vertexExists[i]) {
+        //            for (int j = 0; j < maxVertices; j++) {
+        //                if (adjacencyMatrix[i][j] != -1) {
+        //                   if (weighted) {
+        //                      System.out.println("edge (" + i + " -> " + j + ") = " + (int)adjacencyMatrix[i][j]);
+//
+        //                 } else {
+        //                    System.out.println("edge (" + i + " -> " + j + ")");
+        //                }
+        //            }
+        //        }
+        //    }
+        //    }
+        // }
+        // for(Edge e:d.getEdges()){
+        //   out.write(e.v+" -> "+e.w);
+        // out.newLine();
+        //}
+        }
+}
