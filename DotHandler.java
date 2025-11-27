1e3f79da0cb5b681a0add9b1188f4b89de995a85
HAJ02 <Haj020206@gmail.com>
2025-11-26 22:56:40 -0500

    DOTHANDLER FINAL

--- DotHandler.java
+++ DotHandler.java
@@ -1,33 +1,34 @@
 import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
+import java.io.IOException;
 
 public class DotHandler {
-    public void writeDot(Graph e) {
-            try(BufferedWriter out= new BufferedWriter(new OutputStreamWriter(new FileOutputStream("Graph.dot")))){
-        out.write("Directed Graph");
-          out.newLine();
-          int maxV = e.getMaxVertices();
-             for (int i = 0; i < maxV; i++) {
-                 if (e.vertexExists(i)) {
+    public void writeDot(Graph e){
+        try (BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("DOTGraph.dot")))) {
+            out.write("Directed Graph");
+            out.newLine();
+            int maxV = e.getMaxVertices();
+            for (int i = 0; i < maxV; i++) {
+                if (e.vertexExists(i)) {
                     for (int j = 0; j < maxV; j++) {
-                      if (e.getEdgeWeight(i,j) != -1) {
-                         if (e.isWeighted()) {
-                             System.out.println("edge (" + i + " -> " + j + ") = " + (int)e.getEdgeWeight(i,j));
-                             out.write(e.v+" -> "+e.w);
-                             out.newLine();
-                        } else {
-                           System.out.println("edge (" + i + " -> " + j + ")");
-                             out.write(e.v+" -> "+e.w);
-                             out.newLine();
-                      }
-                   }
-               }
+                        if (e.getEdgeWeight(i, j) != -1) {
+                            if (e.isWeighted()) {
+                                System.out.println("edge (" + i + " -> " + j + ") = " + (int) e.getEdgeWeight(i, j));
+                                out.write(i + " -> " + j);
+                                out.newLine();
+                            } else {
+                                System.out.println("edge (" + i + " -> " + j + ")");
+                                out.write(i + " -> " + j);
+                                out.newLine();
+                            }
+                        }
+                    }
+                }
             }
-           }
         }
-         for(Edge e:d.getEdges()){
-
+        catch (IOException b){
+            b.printStackTrace();
         }
-        }
+    }
 }
